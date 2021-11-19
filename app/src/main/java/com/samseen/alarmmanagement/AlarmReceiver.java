a462a0b535608819af969b606c80b270e0a9e81f
Samson Akanbi <samson.akanbi@outlook.com>
2021-11-19 22:57:15 +0100

    Completed the Alarm Manager section.

--- app/src/main/java/com/samseen/alarmmanagement/AlarmReceiver.java
+++ app/src/main/java/com/samseen/alarmmanagement/AlarmReceiver.java
@@ -4,9 +4,25 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        Intent i = new Intent(context, DestinationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "foxandroid")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Main Alarm Manager")
+                .setContentText("Subscribe for android related content")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(123, builder.build());
     }
 }
